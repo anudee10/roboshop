ecd06636465255864ece73012e80856e37735617
anudeepreddy <anudeepreddy119@gmail.com>
2023-08-23 11:53:40 +0530

    abc

---
+++ catalogue.sh
@@ -0,0 +1,62 @@
+#!/bin/bash
+
+DATE=$(date +%f-%H-%M-%S)
+script_name=$0
+LOGFILE=/tmp/$script_name-$DATE.log
+
+R="\e[31m"
+G="\e[32m"
+N="\e[0m"
+
+USERID=$(id -u)
+
+VALIDATE () {
+if [ $1 -ne 0 ]
+ then 
+   echo -e  $R"$2 is  failure $N"
+   exit 1
+ else
+   echo -e $G"$2 is  success $N"
+ fi
+}
+if [ $USERID -ne 0 ]
+then  
+  echo "please check root access"
+  exit 1
+fi
+
+curl -sL https://rpm.nodesource.com/setup_lts.x | bash &>>$LOGFILE
+
+yum install nodejs -y  &>>$LOGFILE
+
+VALIDATE $? "NODEJS INSTALLED"
+
+useradd roboshop  &>>$LOGFILE
+
+mkdir /app  &>>$LOGFILE
+
+curl -o /tmp/catalogue.zip https://roboshop-builds.s3.amazonaws.com/catalogue.zip   &>>$LOGFILE
+
+cd /app   &>>$LOGFILE
+
+unzip /tmp/catalogue.zip  &>>$LOGFILE
+
+npm install   &>>$LOGFILE
+
+VALIDATE $? "NPM INSTALLED"
+
+cp /home/centos/roboshop /etc/systemd/system/catalogue.service  &>>$LOGFILE
+
+systemctl daemon-reload  &>>$LOGFILE
+
+systemctl enable catalogue  &>>$LOGFILE
+
+systemctl start catalogue  &>>$LOGFILE
+
+cp  /home/centos/roboshop /etc/yum.repos.d/mongo.repo  &>>$LOGFILE
+
+yum install mongodb-org-shell -y  &>>$LOGFILE
+
+VALIDATE $? "SHELL INSTALLED"
+
+mongo --host mongodb.pracricedevops.online </app/schema/catalogue.js  &>>$LOGFILE

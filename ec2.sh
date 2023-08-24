8e650994cf5288dafca8444ef33ff134bed7c803
anudeepreddy <anudeepreddy119@gmail.com>
2023-08-24 21:18:16 +0530

    adding instances

--- ec2.sh
+++ ec2.sh
@@ -5,30 +5,30 @@ INSTANCE_TYPE=""
 IMAGE_ID=ami-03265a0778a880afb
 SECURITY_GROUP_ID=sg-079d47d85da489f20
 DOMAIN_NAME=pracricedevops.online
+ echo "instance:$NAMES"
 
+# #for i in "${NAMES[@]}"
+# do  
+#     if [[ $i == "mongodb" || $i == "mysql" ]]
+#     then
+#         INSTANCE_TYPE="t3.medium"
+#     else
+#         INSTANCE_TYPE="t2.micro"
+#     fi
+#     echo "creating $i instance"
+#     IP_ADDRESS=$(aws ec2 run-instances --image-id $IMAGE_ID  --instance-type $INSTANCE_TYPE --security-group-ids $SECURITY_GROUP_ID --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$i}]" | jq -r '.Instances[0].PrivateIpAddress')
+#     echo "created $i instance: $IP_ADDRESS"
 
-for i in "${NAMES[@]}"
-do  
-    if [[ $i == "mongodb" || $i == "mysql" ]]
-    then
-        INSTANCE_TYPE="t3.medium"
-    else
-        INSTANCE_TYPE="t2.micro"
-    fi
-    echo "creating $i instance"
-    IP_ADDRESS=$(aws ec2 run-instances --image-id $IMAGE_ID  --instance-type $INSTANCE_TYPE --security-group-ids $SECURITY_GROUP_ID --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$i}]" | jq -r '.Instances[0].PrivateIpAddress')
-    echo "created $i instance: $IP_ADDRESS"
-
-    aws route53 change-resource-record-sets --hosted-zone-id Z01639643PLKUYI0BKM8 --change-batch '
-    {
-            "Changes": [{
-            "Action": "CREATE",
-                        "ResourceRecordSet": {
-                            "Name": "'$i.$DOMAIN_NAME'",
-                            "Type": "A",
-                            "TTL": 300,
-                            "ResourceRecords": [{ "Value": "'$IP_ADDRESS'"}]
-                        }}]
-    }
-    '
-done
+#     aws route53 change-resource-record-sets --hosted-zone-id Z01639643PLKUYI0BKM8 --change-batch '
+#     {
+#             "Changes": [{
+#             "Action": "CREATE",
+#                         "ResourceRecordSet": {
+#                             "Name": "'$i.$DOMAIN_NAME'",
+#                             "Type": "A",
+#                             "TTL": 300,
+#                             "ResourceRecords": [{ "Value": "'$IP_ADDRESS'"}]
+#                         }}]
+#     }
+#     '
+# done

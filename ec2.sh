0549c23cc7a68eeebf6065be85d081b7ff7ec682
anudeepreddy <anudeepreddy119@gmail.com>
2023-08-29 10:22:10 +0530

    abc

--- ec2.sh
+++ ec2.sh
@@ -20,19 +20,19 @@ do
         INSTANCE_TYPE="t2.micro"
     fi
     echo "creating $i instance"
-    # IP_ADDRESS=$(aws ec2 run-instances --image-id $IMAGE_ID  --instance-type $INSTANCE_TYPE --security-group-ids $SECURITY_GROUP_ID --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$i}]" | jq -r '.Instances[0].PrivateIpAddress')
-    # echo "created $i instance: $IP_ADDRESS"
+    IP_ADDRESS=$(aws ec2 run-instances --image-id $IMAGE_ID  --instance-type $INSTANCE_TYPE --security-group-ids $SECURITY_GROUP_ID --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$i}]" | jq -r '.Instances[0].PrivateIpAddress')
+    echo "created $i instance: $IP_ADDRESS"
 
-    # aws route53 change-resource-record-sets --hosted-zone-id Z01639643PLKUYI0BKM8 --change-batch '
-    # {
-    #         "Changes": [{
-    #         "Action": "CREATE",
-    #                     "ResourceRecordSet": {
-    #                         "Name": "'$i.$DOMAIN_NAME'",
-    #                         "Type": "A",
-    #                         "TTL": 300,
-    #                         "ResourceRecords": [{ "Value": "'$IP_ADDRESS'"}]
-    #                     }}]
-    # }
-    # '
+    aws route53 change-resource-record-sets --hosted-zone-id Z01639643PLKUYI0BKM8 --change-batch '
+    {
+            "Changes": [{
+            "Action": "CREATE",
+                        "ResourceRecordSet": {
+                            "Name": "'$i.$DOMAIN_NAME'",
+                            "Type": "A",
+                            "TTL": 300,
+                            "ResourceRecords": [{ "Value": "'$IP_ADDRESS'"}]
+                        }}]
+    }
+    '
 done
